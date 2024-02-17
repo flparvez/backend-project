30a2b774b1b37d5387a688fede5ab448e4d36a04
Md Parvez <flparvez23@gmail.com>
2024-02-17 17:18:48 +0600

    update cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = new express();
 
 app.use(
     cors({
-        origin: "*",
+        origin: process.env.CORS_ORIGIN,
         credentials: true,
     })
 );
@@ -16,8 +16,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(morgan("dev")); 
-//HTTP request logger middleware for node.js 
+app.use(morgan("dev")); //HTTP request logger middleware for node.js 
 
 
 
