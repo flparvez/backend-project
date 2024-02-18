bde6349826acfa159820b31850b90e8d1f059cf3
Md Parvez <flparvez23@gmail.com>
2024-02-18 20:10:04 +0600

    final comit

--- src/app.js
+++ src/app.js
@@ -1,30 +1,23 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-// import morgan from "morgan"
-const app = express()
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
-const whitelist = ['http://localhost:3001', 'https://yt-backned.vercel.app'];
+const app = new express();
 
-// ✅ Enable pre-flight requests
-app.options('*', cors());
-
-const corsOptions = {
-  credentials: true,
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(morgan("dev")); //HTTP request logger middleware for node.js 
+
 
 
 //routes import
@@ -38,7 +31,6 @@ import videoRouter from "./routes/video.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
-import morgan from "morgan";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
@@ -52,4 +44,4 @@ app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
 
 
-export { app }
+export default app;
